a14dc3833a94c18c73aaa4929e099340591521d1
alicjaz <alza408@gmail.com>
2023-01-24 17:55:46 +0100

    Competitor's names are printed in a list

--- src/main/java/com/techrecipes/file/json/controller/CompetitorController.java
+++ src/main/java/com/techrecipes/file/json/controller/CompetitorController.java
@@ -1,18 +1,17 @@
 package com.techrecipes.file.json.controller;
 
-import com.techrecipes.file.json.Competitor;
 import com.techrecipes.file.json.Event;
 import com.techrecipes.file.json.EventPrinter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class CompetitorController {
 
     @Autowired
@@ -23,8 +22,10 @@ public class CompetitorController {
     }
 
     @GetMapping("/teams/{competitionName}")
-    public List<String> printCompetitorNames(@PathVariable String competitionName) {
-        return eventPrinter.printCompetitorNames(competitionName);
+    public String printCompetitorNames(@PathVariable String competitionName, Model model) {
+        List<String> competitorNames = eventPrinter.printCompetitorNames(competitionName);
+        model.addAttribute("teams", competitorNames);
+        return "competitors";
     }
 
 }
